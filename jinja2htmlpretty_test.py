177e3597200aa55e717b3d2ee1c492fc688c97f9
Vitaly Kezlya <vitaly@kezlya.com>
2016-09-09 13:14:15 -0500

    test for removing spaces around =

--- jinja2htmlpretty_test.py
+++ jinja2htmlpretty_test.py
@@ -35,6 +35,18 @@ class TestHtmlPretty(unittest.TestCase):
         # Assert
         self._compare_html(expected, result)
 
+    def test_around_equal(self):
+        # Arrange
+        html = '''<a href  \n\t\r  = \n\t\r "#"> blah  \n\t\r =  \n\t\rblah .</a>'''
+        expected = '<a href="#" class="t">.</a>'
+
+        # Act
+        tpl = self.env.from_string(html)
+        result = tpl.render()
+
+        # Assert
+        self._compare_html(expected, result)
+
     def test_final_output(self):
 
         # Arrange
